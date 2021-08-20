98c20a1b4cd69580c7535cae430dc21068d38324
Zhenya <kuprikbeer@gmail.com>
2021-08-20 21:20:50 +0300

    fix: i fix this task)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (Array.isArray(array) && array.length > 0) {
+    let sort1 = array.sort((a, b) => a - b)
+    console.log(sort1[0], sort1)
+    return sort1[0]
+  }
+  else {
+    console.log(0)
+    return 0;
+  }
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (Array.isArray(array) && array.length > 0) {
+    let sort2 = array.sort((a, b) => b - a)
+    console.log(sort2[0], sort2)
+    return sort2[0]
+  }
+  else {
+    console.log(0)
+    return 0;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (Array.isArray(array) && array.length > 0) {
+    let sum = 0
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i]
+    }
+    console.log(sum / array.length)
+    return sum / array.length
+  }
+  else {
+    console.log(0)
+    return 0;
+  }
 }
